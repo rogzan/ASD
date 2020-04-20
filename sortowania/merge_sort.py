b116c944fa4e4be33bfc647fa5b9a6f58474ca73
rogzan <58004469+rogzan@users.noreply.github.com>
2020-04-20 20:33:15 +0200

    Create merge_sort.py

---
+++ sortowania/merge_sort.py
@@ -0,0 +1,36 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements
+        R = arr[mid:]  # into 2 halves
+
+        mergeSort(L)  # Sorting the first half
+        mergeSort(R)  # Sorting the second half
+        merge(arr, L, R)
+
+def merge(arr, L, R):
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+arr=[2,3,9,12,1,0,4,15,7,4,9,5]
+mergeSort(arr)
+print(arr)
