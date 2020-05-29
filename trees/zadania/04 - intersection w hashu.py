4aec254798bc5328d673e0bd0954dd9c336a2cfc
rogzan <58004469+rogzan@users.noreply.github.com>
2020-05-29 15:56:46 +0200

    Create 04 - intersection w hashu.py

---
+++ trees/zadania/04 - intersection w hashu.py
@@ -0,0 +1,7 @@
+def intersection(A,B):
+    counter =0
+    for element in (A):
+        if element.state == BUSY:
+            if find(B, element.key):
+                counter+=1
+    return counter
