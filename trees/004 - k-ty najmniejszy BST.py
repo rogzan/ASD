2f46e2b4bb0132b5ab6f833212f0cf304902376f
rogzan <58004469+rogzan@users.noreply.github.com>
2020-05-29 19:12:42 +0200

    Create 004 - k-ty najmniejszy BST.py

---
+++ trees/004 - k-ty najmniejszy BST.py
@@ -0,0 +1,11 @@
+def kth_smallest(root, k):
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.key
+        root = root.right
