386350145d0df6d5754531611527dc5c1b9b0d12
rogzan <58004469+rogzan@users.noreply.github.com>
2020-06-03 11:55:57 +0200

    Update 16 - poprawnosc haszowania.py

--- trees/zadania/16 - poprawnosc haszowania.py
+++ trees/zadania/16 - poprawnosc haszowania.py
@@ -1,6 +1,14 @@
 # /* Funkcja sprawdza, czy dana tablica haszujaca ma prawidlowo wpisane dane.
 # Haszujemy stringi z tablicy *key, tablica haszujaca to *data, zajetosc *data sprawdza *free, a sama *data przechowuje wartosci haszy. */
 
+# Żeby zrobić to wzorcowo, musimy dla każdego elementu w tablicy znaleźć, gdzie powinien być jego hash 
+# (O(1) wywołanie hash(x)) i sprawdzić, czy między nim a jego hashem jest -1. Tworzymy sobie dodatkową tablicę P,
+# gdzie P[i] oznacza indeks ostatniej napotkanej -1, na lewo od i. Tablicę P uzupełniamy tak, że iterujemy po T 
+# i jak nie ma -1 pod T[i], to wpisujemy indeks ostatniej zapamiętanej -1,  a jak T[i] jest -1, to aktualizujemy
+# indeks ostatniej napotkanej -1. Dla liczb w T, poprzedzających pierwszą -1, możemy w P[i] wpisać jakieś -1 albo None.
+# I teraz jeżeli i < hash(T[i]%N) to sprawdzamy, czy P[hash(i)%N] > i jak tak to jest źle, bo jest -1 między pozycją i hashem.
+# No i przypadek symetryczny dla i > hash(T[i]%N).
+
 class HT:
     def __init__(self):
         self.key = None #tablica na klucze danych
